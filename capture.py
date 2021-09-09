113691cfc492ad2fc6becbb6bad5317c20032bce
scottwong1110 <54442989+scottwong1110@users.noreply.github.com>
2021-09-09 10:50:22 +0800

    Update capture.py

--- capture.py
+++ capture.py
@@ -5,17 +5,17 @@ ipList=os.environ[IPLIST]
 ips=ipList.split(',')
 while True:
     now = datetime.datetime.now()
-    if now.hour == 18 and now.minute = 0 
-      #TODO request
-      print(now.hour)
-      print(now.minute)
-      for ip in ips:
-          rtsp = 'rtsp://admin:1234567a@%s/Streaming/Channels/101' % ip
-          cap = cv2.VideoCapture(rtsp)
-          if cap.isOpened():
-              ret, frame = cap.read()
-              if ret==True:
-                  cv2.imshow(frame)
-                  cv2.waitKey(1)
+    if now.second == 0:
+    #if now.hour == 18 and now.minute==0 and now.second==0
+        print(now.hour)
+        print(now.minute)
+        for ip in ips:
+            rtsp = 'rtsp://admin:1234567a@%s/Streaming/Channels/101' % ip
+            cap = cv2.VideoCapture(rtsp)
+            if cap.isOpened():
+                ret, frame = cap.read()
+                if ret==True:
+                    cv2.imshow(frame)
+                    cv2.waitKey(1)
           
  
