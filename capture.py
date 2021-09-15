cd028f74ec75d50692c6c8ccbb46ca96962085c0
scottwong1110 <54442989+scottwong1110@users.noreply.github.com>
2021-09-15 15:26:46 +0800

    Update capture.py

--- capture.py
+++ capture.py
@@ -3,8 +3,35 @@ import os
 import datetime
 ipList=os.environ['IPLIST']
 ips=ipList.split(',')
+getCertUrl=ps.environ['GET_CERT_URL']
 
-def main():
+from urllib3 import encode_multipart_formdata
+import sys
+import requests
+
+def savePic(rtsp):       
+    cap = cv2.VideoCapture(rtsp)
+    if cap.isOpened():
+        ret, frame = cap.read()
+        if ret==True:
+            cv2.imwrite(ip.replace('.','')+'.jpg',frame)
+
+def getCertificate():
+    data = {
+        orgId='',
+        queryDate='',
+        equipmentSn=''
+    }
+    r = requests.post(getCertUrl,data = data)
+    result =r.json()
+    url = result['returnData']['url']
+    
+    
+def uploadImage():
+    
+    
+
+def main(test=False):
     while True:
         now = datetime.datetime.now()
         if now.second == 0:
@@ -13,11 +40,12 @@ def main():
             print(now.minute)
             for ip in ips:
                 rtsp = 'rtsp://admin:a123456789@%s/Streaming/Channels/101' % ip
-                cap = cv2.VideoCapture(rtsp)
-                if cap.isOpened():
-                    ret, frame = cap.read()
-                    if ret==True:
-                        cv2.imwrite('./test.jpg',frame)
+                savePic(rtsp)
+                
+                getToken(getCertificate)
+            for ip in ips:
+                uploadImage()
+                
                         
                        
 if __name__ == '__main__':
