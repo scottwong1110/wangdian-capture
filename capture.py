7fa7e54313ebca783d04d4dde123cf7bb4a98e25
scottwong1110 <54442989+scottwong1110@users.noreply.github.com>
2021-09-18 16:23:55 +0800

    Update capture.py

--- capture.py
+++ capture.py
@@ -39,13 +39,23 @@ def getCertificate(equipSn):
         'equipmentSn':equipSn
     }
     r = requests.post(getCertUrl,data = data, verify=verify)
+    print(r.text)
     result =json.loads(r.text)
     url = result['returnData']['url']
     return url
     
 def uploadImage(equipSn):
     upload_url  = getCertificate(equipSn)
-    data = {'file':("file", open(equipSn+'.jpg','rb').read())}
+    weFileToken = upload_url.split('&weFileToken=')[1]
+    upload_url=upload_url.split('&weFileToken=')[0]
+    print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+    print('upload_url:',upload_url)
+    print('wefiletoken=',weFileToken)
+    data = {'file':("file", open(equipSn+'.jpg','rb').read()),
+            'isCover':1,
+            'fileName':equipSn,
+            'weFileToken':weFileToken
+            }
     encode_data = encode_multipart_formdata(data)
     data = encode_data[0]
     headers = {
@@ -53,29 +63,33 @@ def uploadImage(equipSn):
     }
     print(datetime.datetime.now().strftime( "%Y-%m-%d %H:%M:%S" )+'start upload')
     r = requests.post(upload_url,headers=headers,data=data,timeout=10, verify=verify)
+    print(r.text)
+    
     result =json.loads(r.text)
     if result['responseCode']=='000000':
         print(datetime.datetime.now().strftime( "%Y-%m-%d %H:%M:%S" )+'finish upload')
-        return result['data']['docId']
+        return result['data']['fileId']
     else:
         return False
     
-def collectData(equipSn,print_time,print_fileId):
-    query = {
-        'networkNo':orgId,
-        'equipSn':equipSn,
-        'printTime':print_time,
-        'printFileId':print_fileId
-    }
-    
-    impressInfoStr = json.dumps(query)
+def collectData(query):
+    query = ','.join(query)
+    impressInfoStr = '['+query+']'
     
     data ={
       'orgId':orgId,
       'impressInfoStr':impressInfoStr
     }
+
+    #data = json.dumps(data)
+
+    print(data)
+    print(collectDataUrl)
+    #headers = {'ContentType':'application/json'}
+    r = requests.post(collectDataUrl,json=data,verify=verify)
     
-    r = requests.post(collectDataUrl,data = data,verify=verify)
+    print('collectData return!!!!!!!!!')
+    print(r.text)
     result =json.loads(r.text)
     print('collect Data return:',result)
     return result['returnData']
@@ -88,6 +102,7 @@ def main():
         #if now.hour == 18 and now.minute==0 and now.second==0
             print('hour:',now.hour)
             print('minute',now.minute)
+            query = []
             for camera in camera_arr:
                 ip = camera['ip']
                 print_time = datetime.datetime.now().strftime( "%Y-%m-%d %H:%M:%S" )
@@ -101,13 +116,19 @@ def main():
                     for i in range(2):
                         print_fileId = uploadImage(camera['equipSn'])
                 if print_fileId!=False:
-                    result = collectDataUrl(camera['equipSn'],print_time,print_fileId)
-                    if result!='success':
-                        print('cannot upload collectData,camera ip=',camera['ip'])    
-                else:
-                    print('cannot upload image,camera ip=',camera['ip'])
+                    query.append(json.dumps({
+                        'networkNo':orgId,
+                        'equipSn':camera['equipSn'],
+                        'printTime':print_time,
+                        'printFileId':print_fileId
+                        }))
+            
+            result = collectData(query)
+            print(result)
+            if result!='success':
+                print('cannot upload collectData,camera ip=',camera['ip'])    
+            else:
+                print('done once')
                                            
 if __name__ == '__main__':
     main()
-
- 
