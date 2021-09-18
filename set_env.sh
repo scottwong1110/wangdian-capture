b6db3d0c34da19c3f39a37470599548876f186e9
scottwong1110 <54442989+scottwong1110@users.noreply.github.com>
2021-09-18 10:40:01 +0800

    Update set_env.sh

--- set_env.sh
+++ set_env.sh
@@ -1,6 +1,6 @@
 #set_env.sh
 export CAMERA_LIST=192.168.66.125#DS-2CD7087FWD-A20190627AACHD35100835
-export GET_CERT_URL=https://rsb-stg.pinan.com.cn/brcp/ac/emp/customer/bank_hall/queryNetworkImpressCert
-export COLLECT_DATA_URL=https://rsb-stg.pinan.com.cn/brcp/ac/emp/customer/bank_hall/insertNetworkImpressInfo
+export GET_CERT_URL=https://rsb-stg.pingan.com.cn/brcp/ac/emp/customer/bank_hall/queryNetworkImpressCert
+export COLLECT_DATA_URL=https://rsb-stg.pingan.com.cn/brcp/ac/emp/customer/bank_hall/insertNetworkImpressInfo
 export ORG_ID=0800
 apt install libgl1-mesa-glx libglib2.0-dev -y
