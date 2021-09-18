37295ac2de73eb2ae33fa826029a4d3a5056b282
scottwong1110 <54442989+scottwong1110@users.noreply.github.com>
2021-09-18 09:32:59 +0800

    Update set_env.sh

--- set_env.sh
+++ set_env.sh
@@ -1,5 +1,5 @@
 #set_env.sh
 export CAMERA_LIST=192.168.66.125#DS-2CD7087FWD-A20190627AACHD35100835
-export GET_CERT_URL=
-export COLLECT_DATA_URL=
+export GET_CERT_URL=https://rsb-stg.pinan.com.cn/brcp/ac/emp/customer/bank_hall/queryNetworkImpressCert
+export COLLECT_DATA_URL=https://rsb-stg.pinan.com.cn/brcp/ac/emp/customer/bank_hall/insertNetworkImpressInfo
 export ORG_ID=0800
