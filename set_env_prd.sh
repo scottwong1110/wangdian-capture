93ad4557df81ac29ffec2201b03b60fecd406b08
scottwong1110 <54442989+scottwong1110@users.noreply.github.com>
2021-09-22 09:29:53 +0800

    Create set_env_prd.sh

---
+++ set_env_prd.sh
@@ -0,0 +1,6 @@
+#set_env.sh
+export CAMERA_LIST=192.168.66.125#DS-2CD7087FWD-A20190627AACHD35100835
+export GET_CERT_URL=https://rsb.pingan.com.cn/brcp/ac/emp/customer/bank_hall/queryNetworkImpressCert
+export COLLECT_DATA_URL=https://rsb.pingan.com.cn/brcp/ac/emp/customer/bank_hall/insertNetworkImpressInfo
+export ORG_ID=0813
+export run_env=PRD
