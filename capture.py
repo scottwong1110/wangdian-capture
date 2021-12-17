bae184940664ebe6dde4f45e9f5bc378475bf787
scottwong1110 <54442989+scottwong1110@users.noreply.github.com>
2021-12-17 16:54:20 +0800

    Update capture.py

--- capture.py
+++ capture.py
@@ -5,24 +5,23 @@ import json
 from urllib.request import urlretrieve
 
 #face_lib_config
-HOUR_list_face = os.environ['HOUR_FACE'].split(',')
-HOURS_face = [int(i) for i in HOUR_list]
-MINUTE_face = int(os.environ['MINUTE_FACE'])
-face_token = os.environ['FACE_TOKEN']
-getFaceListUrl = os.environ['GET_FACE_LIST_URL']
+#HOUR_list_face = os.environ['HOUR_FACE'].split(',')
+#HOURS_face = [int(i) for i in HOUR_list]
+#MINUTE_face = int(os.environ['MINUTE_FACE'])
+#face_token = os.environ['FACE_TOKEN']
+#getFaceListUrl = os.environ['GET_FACE_LIST_URL']
 #aibee interface
-getGroupUrl = os.environ['AIBEE_HOST_URL']+'/users/v1/list-user'
-updateFaceUrl = os.environ['AIBEE_HOST_URL']+'/users/v1/add'
-deleteFaceUrl = os.environ['AIBEE_HOST_URL']+'/users/v1/remove-image'
+#getGroupUrl = os.environ['AIBEE_HOST_URL']+'/users/v1/list-user'
+#updateFaceUrl = os.environ['AIBEE_HOST_URL']+'/users/v1/add'
+#deleteFaceUrl = os.environ['AIBEE_HOST_URL']+'/users/v1/remove-image'
 
-face_list = []
+#face_list = []
 #face_list['wangshengyu345']={'imageUrl':'','updatedTime':'',"isUm":""}
 
 #wangdian_capture_config
-HOUR_list = os.environ['HOUR'].split(',')
-HOURS = [int(i) for i in HOUR_list]
-MINUTE = int(os.environ['MINUTE'])
-
+#HOUR_list = os.environ['HOUR'].split(',')
+#HOURS = [int(i) for i in HOUR_list]
+#MINUTE = int(os.environ['MINUTE'])
 
 run_env = os.environ['RUN_ENV']
 RTSP_KEY = os.environ['RTSP_KEY']
@@ -109,160 +108,47 @@ def collectData(query):
     result =json.loads(r.text)
     return result['returnData']
 
-def updateFace(um,face_obj):
-    data = {
-        "user":{
-            'user_id':um,
-            'imageUrl': face_obj['imageUrl'],
-        },
-        "groups":[
-            run_env
-        ],
-        "check": True
-    }
-    print(json.dumps(data))
-    r = requests.post(updateFaceUrl,json = json.dumps(data))
-    result =json.loads(r.text)
-    if result['error_no']==0:
-        print('update face pic successfully!,um='+um)
-
-def deleteFace(um):
-    data = {
-        "user_id":um,
-        "group_id":run_env
-    } 
-    print(json.dumps(data))
-    r = requests.post(deleteFaceUrl,json = json.dumps(data))
-    result =json.loads(r.text)
-    if result['error_no']==0:
-        print('delete face pic successfully!,um='+um)
-
-
-def saveFacePic(um,imageUrl):
-    urlretrieve(imageUrl, os.path.join('face_pic',um+'.jpg'))  
-
-def getRunningFaceList(group_id):
-    data = {
-        "group_id":group_id
-    } 
-    print(json.dumps(data))
-    r = requests.post(getGroupUrl,json = json.dumps(data))
-    result =json.loads(r.text)
-    if result['error_no']==0:
-        print('group user number:'+len(result['data']['list']))
-        print('group user:'+result['data']['list'])
-
-def getBranchFaceListAndUpdate(orgId):
-    data = {
-        'branchNo':orgId,
-        'token':face_token
-    }
-    r = requests.post(getFaceListUrl,data = data, verify=verify)
-    print(r.text)
-    result =json.loads(r.text)
-    updatedTime = 0
-    for data in result['data']:
-        if data['status']=='0':
-            newUm = data['umCode'] 
-            #not existed
-            if newUm not in face_list.keys():
-                face_list[newUm] = {'imageUrl':data['imageUrl'],'updatedTime':data['updatedTime'],"isUm":data['isUm']}
-                #save picture and update
-                saveFacePic(newUm,data['imageUrl'])
-                updateFace(newUm,face_list[newUm])
-
-            else:
-                #newer info
-                if data['updatedTime'] > face_list[newUm]['updatedTime']:
-                    face_list[newUm] = {'imageUrl':data['imageUrl'],'updatedTime':data['updatedTime'],"isUm":data['isUm']}
-                    #save picture and update
-                    saveFacePic(newUm,data['imageUrl'])
-                    updateFace(newUm,face_list[newUm])
-    #need deletion
-    for key in face_list:
-        delete = 1
-        for data in result['data']:
-            if data['status']=='0':
-                if data['umCode'] == key:
-                    delete = 0
-        if delete == 1:
-            deleteFace(key)
-
-    #show running face list
-    getRunningFaceList(run_env)
-
-    #return faceList
-
-    
-
-
 def main():
-    while True:
-        now = datetime.datetime.now()
-
-        #face lib function
+    query = []
+    for camera in camera_arr:
+        ip = camera['ip']
+        print('start get picture from ip:',ip)
+        print_time = datetime.datetime.now().strftime( "%Y-%m-%d %H:%M:%S" )
+        rtsp = 'rtsp://admin:'+RTSP_KEY+'@%s/Streaming/Channels/101' % ip
         try:
-            for HOUR in HOURS_face:
-                if now.hour == HOUR and now.minute % MINUTE_face == 0 and now.second==0:
-                    print('face_lib')
-                    print('hour:',now.hour)
-                    print('minute',now.minute)
-                    try :
-                        getBranchFaceListAndUpdate(orgId)
-                    except Exception as e:
-                        print('error from getBranchFaceList')
-                        print(e)
+            savePic(rtsp,camera['equipSn'])
         except Exception as e:
-            print('error from face_lib')
             print(e)
+            print('cannot capture image',camera['ip'])
+        try:
+            print_fileId = uploadImage(camera['equipSn'])
 
-        #wangdian capture function
-        for HOUR in HOURS:
-            if now.hour == HOUR and now.minute % MINUTE == 0 and now.second==0:
-            #if now.hour == 18 and now.minute==0 and now.second==0
-                print('wangdian_capture')
-                print('hour:',now.hour)
-                print('minute',now.minute)
-                query = []
-                for camera in camera_arr:
-                    ip = camera['ip']
-                    print('start get picture from ip:',ip)
-                    print_time = datetime.datetime.now().strftime( "%Y-%m-%d %H:%M:%S" )
-                    rtsp = 'rtsp://admin:'+RTSP_KEY+'@%s/Streaming/Channels/101' % ip
-                    try:
-                        savePic(rtsp,camera['equipSn'])
-                    except Exception as e:
-                        print(e)
-                        print('cannot capture image',camera['ip'])
-                    try:
-                        print_fileId = uploadImage(camera['equipSn'])
-
-                        if print_fileId==False:
-                            print_fileId = uploadImage(camera['equipSn'])
-                        if print_fileId!=False:
-                            query.append(json.dumps({
-                                'networkNo':orgId,
-                                'equipSn':camera['equipSn'],
-                                'printTime':print_time,
-                                'printFileId':print_fileId
-                            }))
-                        print('finished get picture from ip:',ip)
-                    except Exception as e:
-                        print(e)
-                        print('cannot upload image',camera['ip'])
-                try:    
-                    result = collectData(query)
-                    if result=='success':
-                        print('!!!!!!!!!!!!!!!!done upload all images once')
-                    else:
-                        result = collectData(query)
-                        if result=='success':
-                            print('!!!!!!!!!!!!!!!!done upload all images once')
-                        else:
-                            print('cannot collect data')
-                except Exception as e:
-                    print(e)
-                    print('cannot collect data')
+            if print_fileId==False:
+                print_fileId = uploadImage(camera['equipSn'])
+            if print_fileId!=False:
+                query.append(json.dumps({
+                    'networkNo':orgId,
+                    'equipSn':camera['equipSn'],
+                    'printTime':print_time,
+                    'printFileId':print_fileId
+                }))
+            print('finished get picture from ip:',ip)
+        except Exception as e:
+            print(e)
+            print('cannot upload image',camera['ip'])
+    try:    
+        result = collectData(query)
+        if result=='success':
+            print('!!!!!!!!!!!!!!!!done upload all images once')
+        else:
+            result = collectData(query)
+            if result=='success':
+                print('!!!!!!!!!!!!!!!!done upload all images once')
+            else:
+                print('cannot collect data')
+    except Exception as e:
+        print(e)
+        print('cannot collect data')
                                            
 if __name__ == '__main__':
     main()
